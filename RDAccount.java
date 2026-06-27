2a130a9ebfc018e82d7227001dd4e058c9196c69
Justeen saron <justeenseron@gmail.com>
2026-06-27 15:56:48 +0530

    Add files via upload

--- RDAccount.java
+++ RDAccount.java
@@ -3,15 +3,16 @@ public class RDAccount extends Account {
     public RDAccount(int tenure, float principal) {
         super(tenure, principal);
     }
-    @Override
     public float calculateAmountDeposited() {
-        return principal * tenure;
+        return principal * tenure * 12;
     }
-    @Override
     public float calculateInterest() {
-        float r = rateOfInterest / 100.0f;
-        int n = 12;
-        float t = tenure / 12.0f;
-        return (float)(principal * (Math.pow((1 + r / n), n * t) - 1));
+        int months = tenure * 12; 
+        float monthlyRate = rateOfInterest / (12 * 100);
+        float maturity = (float) (principal * ((Math.pow(1 + monthlyRate, months) - 1) / monthlyRate) * (1 + monthlyRate));
+        return maturity - calculateAmountDeposited();
+    }
+    public float calculateMaturityAmount(float deposited, float interest) {
+        return deposited + interest;
     }
 }
